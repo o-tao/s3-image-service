a3f6449887e7ae49dbfb871e03e168fbd7e94af5
o-tao <dhtkdals369@gmail.com>
2025-05-06 19:26:15 +0900

    chore: 설명 추가

--- src/main/java/example/domain/BaseEntity.java
+++ src/main/java/example/domain/BaseEntity.java
@@ -2,6 +2,7 @@ package example.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,14 +12,17 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class BaseEntity {
 
+    @Comment("기본 키")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Comment("생성일자")
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @Comment("수정일자")
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
